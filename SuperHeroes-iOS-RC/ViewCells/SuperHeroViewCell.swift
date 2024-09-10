a9a6fb5e452d3fbf97a5dcba6e0fdcbaf1ebe09d
rolando_cf <rolandodj83@gmail.com>
2024-09-10 14:04:19 +0200

    Se ha creado 2 funciones para que los bordes de la imagen se vean redondeados. HAY PDF.

--- SuperHeroes-iOS-RC/ViewCells/SuperHeroViewCell.swift
+++ SuperHeroes-iOS-RC/ViewCells/SuperHeroViewCell.swift
@@ -14,6 +14,8 @@ class SuperHeroViewCell: UITableViewCell {
     
     @IBOutlet weak var nameLabel: UILabel!
     
+    @IBOutlet weak var cardView: UIView!
+    
     func render(superHero: SuperHero) {
         nameLabel.text = superHero.name
         avatarImageView.loadFrom(url: superHero.image.url)
@@ -22,6 +24,7 @@ class SuperHeroViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        cardView.roundCorners(radius: 8)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
